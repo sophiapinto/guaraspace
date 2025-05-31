ea98fceccd7b924675dcdbc285f19f6f7ec43fb7
sophiapinto <sarahsophiapinto@hotmail.com>
2025-05-31 10:00:49 -0300

    Update portfólio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,15 +13,13 @@ export default class Porfolio extends Component {
               return(
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
-                    <a href="#modal-01">
-                      <img src={`${item.imgurl}`} className="item-img"/>
+                      <img src={`${item.imgurl}`} className="item-img" alt='img-port'/>
                       <div className="overlay">
                         <div className="portfolio-item-meta">
                           <h5>{item.name}</h5>
                           <p>{item.description}</p>
                         </div>
                       </div>
-                    </a>
                   </div>
                 </div>
               )
